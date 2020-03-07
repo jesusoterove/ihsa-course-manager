83d3c5d7556ef2e51ec70bb2f44b170d177d8c7d
jesusoterove <jesusoterove@gmail.com>
2020-03-07 05:58:51 -0500

    Updated angular from 7.2 to 8.2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'ihsa-cours-manager';
+  title = 'ihsa-course-manager';
 }
