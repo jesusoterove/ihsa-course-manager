9b3be26cb51b05bb5860bef58aa86424ac27a882
jesusoterove <jesusoterove@gmail.com>
2020-03-08 19:55:46 -0500

    Fixed to deploy on git

--- server.js
+++
@@ -1,8 +0,0 @@
-app.use(express.static("./dist/ihsa-cours-manager"));
-
-
-app.get('/*', function(req, res) {
-  res.sendFile('index.html', {root: 'dist/ihsa-cours-manager/'});
-});
-
-app.listen(process.env.PORT || 8080);
